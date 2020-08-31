c19953bdbdc481583206913e61d0e19adbeab0fe
YeinHeo <yeinheo95@gmail.com>
2020-08-31 21:33:02 +0900

    baekjoon

--- baekjoon/10814.py
+++ baekjoon/10814.py
@@ -2,17 +2,11 @@ n = int(input())
 
 a = []
 for i in range(n):
-    x = list(input().split())
-    a.append(x)
+    x, y = list(input().split())
+    a.append((int(x), y))
 
-b = dict()
-c = dict()
+b = sorted(a, key=lambda item: item[0])
 
-for i in range(n):
-    b[i] = int(a[i][0])
-    c[i] = a[i][1]
+for i in b:
+    print(str(i[0]), i[1])
 
-d = dict(sorted(b.items(), key=lambda item: item[1]))
-
-for key, value in d.items():
-    print(str(value) + ' ' + c[key])
